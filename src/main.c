8f6abf6a63b8a54efef3e5f40ab48475ef7b5e1b
Mark Devenyi <markdevenyidev@gmail.com>
2026-06-08 18:19:51 -0400

    feat: add usage information

--- src/main.c
+++ src/main.c
@@ -9,6 +9,12 @@ int main(int argc, char* argv[]) {
     (void) argc;
     (void) argv;
 
+    if (argc != 2) {
+        printf("mdprev\n\n");
+        printf("Usage: mdprev <markdown file>\n");
+        return 0;
+    }
+        
     GENPATH = argv[1];
     mdprev_host(12345, GENBODY);
 
