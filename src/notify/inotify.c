07dc0e1d43b81931a632fdda42355de58e87eb12
Mark Devenyi <markdevenyidev@gmail.com>
2026-06-08 18:00:32 -0400

    feat(notify): subscribe to directory events and not file events

--- src/notify/inotify.c
+++ src/notify/inotify.c
@@ -1,6 +1,8 @@
+#include <libgen.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <sys/inotify.h>
@@ -11,17 +13,20 @@ static int32_t ifd = -1;
 static int32_t wfd = -1;
 
 int32_t track(void) {
-    ifd = inotify_init();
+    ifd = inotify_init1(IN_NONBLOCK | IN_CLOEXEC);
     if (ifd == -1) {
         perror("inotify_init()");
         exit(1);
     }
 
-    wfd = inotify_add_watch(ifd, GENPATH, IN_CLOSE_WRITE);
+    char* gpath = strdup(GENPATH);
+    char* dir = dirname(gpath);
+    wfd = inotify_add_watch(ifd, dir, IN_CLOSE_WRITE);
     if (wfd == -1) {
         perror("inotify_add_watch()");
     }
 
+    free(gpath);
     return ifd;
 }
 
